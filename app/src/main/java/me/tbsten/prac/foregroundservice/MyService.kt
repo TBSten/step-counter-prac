43c11faf18eaf9431f44f817d43aedfeaa74f147
TBSten <81161390+TBSten@users.noreply.github.com>
2024-06-25 00:48:38 +0900

    MyServiceに歩数カウント機能を実装

--- app/src/main/java/me/tbsten/prac/foregroundservice/MyService.kt
+++ app/src/main/java/me/tbsten/prac/foregroundservice/MyService.kt
@@ -1,41 +1,39 @@
 package me.tbsten.prac.foregroundservice
 
-import android.app.NotificationManager
-import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
-import android.util.Log
 import androidx.core.app.ServiceCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 private const val NOTIFICATION_CHANNEL_ID = "my-service-notification-channel"
 
 class MyService : LifecycleService() {
+    private val counter = SensorStepCounter(this, lifecycleScope)
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             startForegroundService()
         }
+        counter.start()
         return super.onStartCommand(intent, flags, startId)
     }
 
-    private val notificationManager by lazy {
-        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    override fun onDestroy() {
+        super.onDestroy()
+        counter.finish()
     }
 
     private fun startForegroundService() {
         val notificationId = (System.currentTimeMillis() % Int.MAX_VALUE).toInt()
         createNotificationChannel(
             id = NOTIFICATION_CHANNEL_ID,
-            name = "カウントアップするやーつー",
+            name = "歩数カウンター",
         )
         val notification =
             createNotification(
                 channelId = NOTIFICATION_CHANNEL_ID,
-                contentTitle = "start count up",
-                contentText = "started count up",
+                contentTitle = "歩数を計測中です",
             )
 
         ServiceCompat.startForeground(
@@ -44,28 +42,5 @@ class MyService : LifecycleService() {
             notification,
             ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH,
         )
-
-        createNotificationChannel(
-            id = NOTIFICATION_CHANNEL_ID,
-            name = "カウントアップするやーつー",
-            importance = NotificationManager.IMPORTANCE_LOW,
-        )
-
-        lifecycleScope.launch {
-            var count = 0
-            while (true) {
-                delay(5_000)
-                notificationManager.notify(
-                    notificationId,
-                    createNotification(
-                        channelId = NOTIFICATION_CHANNEL_ID,
-                        contentTitle = "count: $count",
-                        contentText = "counted up to $count",
-                    )
-                )
-                Log.d("prac-foreground-service", "count:$count")
-                count++
-            }
-        }
     }
 }
